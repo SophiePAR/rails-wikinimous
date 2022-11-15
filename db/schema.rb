a97796c362c40c4b55d46bf867c7b0df04040d9d
sophie Parisot <sophie.parisot.93@gmail.com>
2022-11-15 23:03:23 +0100

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -17,5 +17,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_142800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating 10 fake...'
+10.times do
+  article = Article.new(
+    title: Faker::TvShows::BigBangTheory.character,
+    content: Faker::TvShows::BigBangTheory.quote
+  )
+  article.save!
+end
+puts 'Finished!'
